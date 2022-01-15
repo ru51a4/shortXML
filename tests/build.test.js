2c59f4e8f23d1b0e79cd67e1d77c707665201a1e
ru51a4 <ru51a4@mail.ru>
2022-01-15 16:42:52 +0500

    bugfix

--- tests/build.test.js
+++ tests/build.test.js
@@ -29,8 +29,7 @@ test('simple', () => {
 });
 test('array', ()=>{
     let obj = JSON.parse(`{"a":["1",{"a":"lol"},"1","1"]}`);
-    let str =`
-<root>
+    let str =`<root>
     <a superxmltoobj_print="true" superxmltoobj_var="12">1</a>
         <a>
             <a>
@@ -40,4 +39,30 @@ test('array', ()=>{
     <a superxmltoobj_print="false" superxmltoobj_var="12"/>
     <a superxmltoobj_print="false" superxmltoobj_var="12"/>
     </root>`
+    expect(superxmltoobj.decode(str)).toEqual(obj);
+});
+test('simple2', ()=>{
+    let obj = JSON.parse(`{
+    "glossary": {
+        "title": "example glossary",
+\t\t"GlossDiv": {
+            "title": "S",
+\t\t\t"GlossList": {
+                "GlossEntry": {
+                    "ID": "SGML",
+\t\t\t\t\t"SortAs": "SGML",
+\t\t\t\t\t"GlossTerm": "Standard Generalized Markup Language",
+\t\t\t\t\t"Acronym": "SGML",
+\t\t\t\t\t"Abbrev": "ISO 8879:1986",
+\t\t\t\t\t"GlossDef": {
+                        "para": "A meta-markup language, used to create markup languages such as DocBook.",
+\t\t\t\t\t\t"GlossSeeAlso": ["GML", "XML"]
+                    },
+\t\t\t\t\t"GlossSee": "markup"
+                }
+            }
+        }
+    }
+}`);
+    expect(superxmltoobj.decode(superxmltoobj.code(obj))).toEqual(obj);
 });
